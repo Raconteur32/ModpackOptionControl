8fa608d37f8a020b6028d6fc013d20c843896944
Raconteur <code-project-contact@paulbrancieq.fr>
2026-05-31 19:59:09 +0200

    entrypoint: prelaunch applique les patchs non encore appliqués à la McInstance

---
+++ src/main/kotlin/fr/raconteur/moc/MocPreLaunchEntrypoint.kt
@@ -0,0 +1,31 @@
+package fr.raconteur.moc
+
+import fr.raconteur.moc.filesystem.McInstanceMocFileSystem
+import fr.raconteur.moc.platform.FabricPlatformService
+import fr.raconteur.moc.platform.PlatformService
+import fr.raconteur.moc.versioning.Patch
+import fr.raconteur.moc.versioning.PatchList
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
+import org.slf4j.LoggerFactory
+
+object MocPreLaunchEntrypoint : PreLaunchEntrypoint {
+    private val logger = LoggerFactory.getLogger("moc")
+
+    override fun onPreLaunch() {
+        PlatformService.INSTANCE = FabricPlatformService
+
+        val applied = McInstanceMocFileSystem.appliedPatches.toSet()
+        val toApply = PatchList.getAll().filter { it !in applied }
+
+        if (toApply.isEmpty()) return
+
+        for (patchName in toApply) {
+            try {
+                McInstanceMocFileSystem.applyPatch(Patch.load(patchName), forceDelete = false)
+                logger.info("[moc] Applied patch: $patchName")
+            } catch (e: Exception) {
+                logger.error("[moc] Failed to apply patch '$patchName': ${e.message}")
+            }
+        }
+    }
+}
